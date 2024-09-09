61a1e93c629f6163fcc23436e061884fe8122386
AT <AT@DESKTOP-AKM319L>
2024-09-09 22:06:09 +0500

    Add project files.

---
+++ GenericsBasicExample/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int x = 12;
+int y = 13;
+Swap(ref x,ref y);
+Console.WriteLine($"x={x}, y={y}");
+
+string firstName = "Bakar";
+string secondName = "Ali";
+
+Swap(ref firstName, ref secondName);
+Console.WriteLine($"firstName={firstName}, secondName={secondName}");
+
+//Generic Function
+void Swap<T>(ref T a, ref T b)
+{
+    T temp = a; a = b; b = temp;
+}
